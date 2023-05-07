87dcc90f01d3c805f26e5d0e6efd1bf6e92c1857
Kevin Coakley <kcoakley@sdsc.edu>
2023-05-07 11:32:53 +0200

    Version 1.0.0 of image_classification
    Signed-off-by: Kevin Coakley <kcoakley@sdsc.edu>

---
+++ code/image_classification/dataset_preprocess.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+
+num_classes = None
+image_shape = None
+
+
+def cifar_preprocessing(image, label):
+    image = tf.cast(image, tf.float32)
+    image = tf.image.per_image_standardization(image)
+    image = tf.image.resize(image, image_shape[:2])
+    return image, tf.squeeze(tf.one_hot(label, depth=num_classes))
+
+
+def cifar_augmentation(image, label):
+    image = tf.image.resize_with_crop_or_pad(
+        image, image_shape[0] + 15, image_shape[1] + 15
+    )
+    image = tf.image.random_crop(image, image_shape, seed=1)
+    image = tf.image.random_flip_left_right(image, seed=1)
+    return image, label
+
+
+def get_cifar_input(X_train, y_train, X_val, y_val, batch_size, shuffle_seed):
+    train_dataset = (
+        tf.data.Dataset.from_tensor_slices((X_train, y_train))
+        .map(cifar_preprocessing)
+        .repeat()
+        .map(cifar_augmentation)
+        .shuffle(10000, seed=shuffle_seed)
+        .batch(batch_size)
+        .prefetch(tf.data.experimental.AUTOTUNE)
+    )
+    val_dataset = (
+        tf.data.Dataset.from_tensor_slices((X_val, y_val))
+        .map(cifar_preprocessing)
+        .batch(200)
+        .prefetch(tf.data.experimental.AUTOTUNE)
+    )
+    return train_dataset, val_dataset
+
+
+def get_dataset(dataset_name, batch_size, shuffle_seed, shape=[32, 32, 3]):
+    global num_classes
+    global image_shape
+    image_shape = shape
+
+    if dataset_name == "cifar10":
+        num_classes = 10
+        (X_train, y_train), (X_val, y_val) = tf.keras.datasets.cifar10.load_data()
+        return get_cifar_input(X_train, y_train, X_val, y_val, batch_size, shuffle_seed)
+    elif dataset_name == "cifar100":
+        num_classes = 100
+        (X_train, y_train), (X_val, y_val) = tf.keras.datasets.cifar100.load_data()
+        return get_cifar_input(X_train, y_train, X_val, y_val, batch_size, shuffle_seed)
---
+++ code/image_classification/image_classification.py
@@ -0,0 +1,377 @@
+import argparse, csv, math, os, random, sys, yaml
+import tensorflow as tf
+import numpy as np
+from datetime import datetime
+import dataset_preprocess
+
+script_version = "1.0.0"
+
+epochs = 50
+batch_size = 128
+learning_rate = 4e-4
+steps_per_epoch = math.ceil(50000 / batch_size)
+
+
+def image_classification(
+    run_number,
+    model_name="Densenet",
+    dataset_name="cifar10",
+    deterministic=False,
+    seed_val=1,
+    run_name="",
+    save_model=False,
+):
+    if deterministic or seed_val != 1:
+        """
+        ## Configure Tensorflow for fixed seed runs
+        """
+        major, minor, revision = tf.version.VERSION.split(".")
+
+        if int(major) >= 2 and int(minor) >= 7:
+            # Sets all random seeds for the program (Python, NumPy, and TensorFlow).
+            # Supported in TF 2.7.0+
+            tf.keras.utils.set_random_seed(seed_val)
+            print("Setting random seed using tf.keras.utils.set_random_seed()")
+        else:
+            # for TF < 2.7
+            random.seed(seed_val)
+            np.random.seed(seed_val)
+            tf.random.set_seed(seed_val)
+            print("Setting random seeds manually")
+        # Configures TensorFlow ops to run deterministically to enable reproducible
+        # results with GPUs (Supported in TF 2.8.0+)
+        if int(major) >= 2 and int(minor) >= 8:
+            tf.config.experimental.enable_op_determinism()
+            print("Enabled op determinism")
+
+    """
+    ## Datasets definition dictionary
+    """
+    datasets = {
+        "cifar100": {
+            "data": tf.keras.datasets.cifar100,
+            "num_class": 100,
+            "input_shape": (128, 128, 3),
+        },
+        "cifar10": {
+            "data": tf.keras.datasets.cifar10,
+            "num_class": 10,
+            "input_shape": (128, 128, 3),
+        },
+    }
+
+    input_shape = datasets[dataset_name]["input_shape"]
+    num_class = datasets[dataset_name]["num_class"]
+
+    """
+    ## Models definition dictionary
+    """
+    models = {
+        "EfficientNet": {
+            "application": tf.keras.applications.EfficientNetB4,
+            "args": {
+                "include_top": True,
+                "weights": None,
+                "input_shape": input_shape,
+                "classes": num_class,
+                "classifier_activation": "softmax",
+            },
+        },
+        "Xception": {
+            "application": tf.keras.applications.Xception,
+            "args": {
+                "include_top": True,
+                "weights": None,
+                "input_shape": input_shape,
+                "classes": num_class,
+                "classifier_activation": "softmax",
+            },
+        },
+        "InceptionV3": {
+            "application": tf.keras.applications.InceptionV3,
+            "args": {
+                "include_top": True,
+                "weights": None,
+                "input_shape": input_shape,
+                "classes": num_class,
+                "classifier_activation": "softmax",
+            },
+        },
+        "ResNet": {
+            "application": tf.keras.applications.resnet_v2.ResNet50V2,
+            "args": {
+                "include_top": True,
+                "weights": None,
+                "input_shape": input_shape,
+                "classes": num_class,
+                "classifier_activation": "softmax",
+            },
+        },
+        "DenseNet": {
+            "application": tf.keras.applications.DenseNet201,
+            "args": {
+                "include_top": True,
+                "weights": None,
+                "input_shape": input_shape,
+                "classes": num_class,
+                "classifier_activation": "softmax",
+            },
+        },
+    }
+
+    """
+    ## Load the dataset
+    """
+    train_dataset, val_dataset = dataset_preprocess.get_dataset(
+        dataset_name, batch_size, shuffle_seed=42, shape=input_shape
+    )
+
+    """
+    ## Create the model
+    """
+    model = tf.keras.Sequential()
+    model.add(models[model_name]["application"](**models[model_name]["args"]))
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+
+    # Print the model summary
+    model.summary()
+
+    """
+    ## Define the learning rate scheduler to decrease the learning rate by 10x every 45 epochs
+    """
+
+    def lr_scheduler(epoch):
+        new_lr = learning_rate * (0.1 ** (epoch // 45))
+        return new_lr
+
+    reduce_lr = tf.keras.callbacks.LearningRateScheduler(lr_scheduler)
+
+    """
+    ## Create the base name for the log and model files
+    """
+    base_name = os.path.basename(sys.argv[0]).split(".")[0]
+
+    if len(run_name) >= 1:
+        base_name = base_name + "_" + run_name
+
+    """
+    ## Define csv logger callback
+    """
+    csv_train_log_file = base_name + "_log_" + str(run_number) + ".csv"
+
+    csv_logger = tf.keras.callbacks.CSVLogger(csv_train_log_file)
+
+    # Define callbacks
+    callbacks = [reduce_lr, csv_logger]
+
+    # Time the training
+    start_time = datetime.now()
+
+    # Train the model
+    model.fit(
+        train_dataset,
+        epochs=epochs,
+        steps_per_epoch=steps_per_epoch,
+        validation_data=val_dataset,
+        callbacks=callbacks,
+    )
+
+    # Calculate the training time
+    end_time = datetime.now()
+    training_time = end_time - start_time
+
+    """
+    ## Evaluate the trained model
+    """
+    score = model.evaluate(val_dataset, steps=steps_per_epoch, verbose=0)
+
+    print("Test loss: ", score[0])
+    print("Test accuracy: ", score[1])
+    print("Training time: ", training_time)
+
+    """
+    ## Save the model
+    """
+    if save_model:
+        y_predicted = model.predict(val_dataset)
+        np.save(base_name + "_predict_" + str(run_number) + ".npy", y_predicted)
+        model.save(base_name + "_model_" + str(run_number) + ".h5")
+
+    return score[0], score[1], epochs, training_time
+
+
+def get_system_info():
+    if os.path.exists("system_info.py"):
+        base_name = os.path.basename(sys.argv[0]).split(".")[0]
+
+        import system_info
+
+        sysinfo = system_info.get_system_info()
+
+        with open("%s.yaml" % base_name, "w") as system_info_file:
+            yaml.dump(sysinfo, system_info_file, default_flow_style=False)
+
+        return sysinfo
+    else:
+        return None
+
+
+def save_score(
+    test_loss,
+    test_accuracy,
+    epochs,
+    training_time,
+    model_name,
+    dataset_name,
+    deterministic,
+    seed_val,
+    run_name="",
+):
+    csv_file = os.path.basename(sys.argv[0]).split(".")[0] + ".csv"
+    write_header = False
+
+    # If determistic is false and the seed value is 1 then the
+    # seed value is totally random and we don't know what it is.
+    if deterministic == False and seed_val == 1:
+        seed_val = "random"
+
+    if not os.path.isfile(csv_file):
+        write_header = True
+
+    with open(csv_file, "a") as csvfile:
+        fieldnames = [
+            "run_name",
+            "script_version",
+            "date_time",
+            "fit_time",
+            "python_version",
+            "tensorflow_version",
+            "tensorflow_compiler_version",
+            "epochs",
+            "model_name",
+            "dataset_name",
+            "random_seed",
+            "test_loss",
+            "test_accuracy",
+        ]
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        if write_header:
+            writer.writeheader()
+
+        writer.writerow(
+            {
+                "run_name": run_name,
+                "script_version": script_version,
+                "date_time": datetime.now(),
+                "fit_time": training_time,
+                "python_version": sys.version.replace("\n", ""),
+                "tensorflow_version": tf.version.VERSION,
+                "tensorflow_compiler_version": tf.version.COMPILER_VERSION,
+                "epochs": epochs,
+                "model_name": model_name,
+                "dataset_name": dataset_name,
+                "random_seed": seed_val,
+                "test_loss": test_loss,
+                "test_accuracy": test_accuracy,
+            }
+        )
+
+
+def parse_arguments(args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--deterministic",
+        dest="deterministic",
+        help="Run in deterministic mode",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--random-seed-val",
+        dest="random_seed_val",
+        help="Pick a random int for the seed value every run and record it in the csv file",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--seed-val", dest="seed_val", help="Set the seed value", type=int, default=1
+    )
+
+    parser.add_argument(
+        "--num-runs",
+        dest="num_runs",
+        help="Number of training runs",
+        type=int,
+        default=5,
+    )
+
+    parser.add_argument(
+        "--run-name",
+        dest="run_name",
+        help="Name of training run",
+        default="",
+    )
+
+    parser.add_argument(
+        "--save-model", dest="save_model", help="Save the model", action="store_true"
+    )
+
+    parser.add_argument(
+        "--model-name",
+        dest="model_name",
+        help="EfficientNet, Xception, InceptionV3, ResNet or DenseNet",
+        default="DenseNet",
+    )
+
+    parser.add_argument(
+        "--dataset-name",
+        dest="dataset_name",
+        help="cifar10, cifar100, fashion_mnist",
+        default="cifar10",
+    )
+
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    args = parse_arguments(sys.argv[1:])
+
+    system_info = get_system_info()
+    seed_val = args.seed_val
+
+    for x in range(args.num_runs):
+        if args.random_seed_val:
+            seed_val = random.randint(0, 2**32 - 1)
+
+        print(
+            "\nImage Classification (%s - %s): %s of %s [%s]\n======================\n"
+            % (args.model_name, args.dataset_name, str(x + 1), args.num_runs, seed_val)
+        )
+        test_loss, test_accuracy, epochs, training_time = image_classification(
+            x + 1,
+            model_name=args.model_name,
+            dataset_name=args.dataset_name,
+            deterministic=args.deterministic,
+            seed_val=seed_val,
+            run_name=args.run_name,
+            save_model=args.save_model,
+        )
+        save_score(
+            test_loss,
+            test_accuracy,
+            epochs,
+            training_time,
+            model_name=args.model_name,
+            dataset_name=args.dataset_name,
+            deterministic=args.deterministic,
+            seed_val=seed_val,
+            run_name=args.run_name,
+        )

6ee1f1b5e829a833566c610fedc8a6691643a0a9
Kevin Coakley <kcoakley@sdsc.edu>
2023-05-18 20:47:20 +0200

    Added cats vs dogs dataset
    Signed-off-by: Kevin Coakley <kcoakley@sdsc.edu>

--- code/image_classification/dataset_preprocess.py
+++ code/image_classification/dataset_preprocess.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
 
 num_classes = None
 image_shape = None
@@ -20,7 +21,7 @@ def augmentation(image, label):
     return image, label
 
 
-def get_input(X_train, y_train, X_val, y_val, batch_size, shuffle_seed):
+def get_input_keras(X_train, y_train, X_val, y_val, batch_size, shuffle_seed):
     train_dataset = (
         tf.data.Dataset.from_tensor_slices((X_train, y_train))
         .map(preprocessing)
@@ -39,6 +40,21 @@ def get_input(X_train, y_train, X_val, y_val, batch_size, shuffle_seed):
     return train_dataset, val_dataset
 
 
+def get_input_tfds(train, val, batch_size, shuffle_seed):
+    train_dataset = (
+        train.map(preprocessing)
+        .repeat()
+        .map(augmentation)
+        .shuffle(10000, seed=shuffle_seed)
+        .batch(batch_size)
+        .prefetch(tf.data.experimental.AUTOTUNE)
+    )
+    val_dataset = (
+        val.map(preprocessing).batch(200).prefetch(tf.data.experimental.AUTOTUNE)
+    )
+    return train_dataset, val_dataset
+
+
 def get_dataset(dataset_name, batch_size, shuffle_seed, shape=[32, 32, 3]):
     global num_classes
     global image_shape
@@ -47,14 +63,21 @@ def get_dataset(dataset_name, batch_size, shuffle_seed, shape=[32, 32, 3]):
     if dataset_name == "cifar10":
         num_classes = 10
         (X_train, y_train), (X_val, y_val) = tf.keras.datasets.cifar10.load_data()
-        return get_input(X_train, y_train, X_val, y_val, batch_size, shuffle_seed)
+        return get_input_keras(X_train, y_train, X_val, y_val, batch_size, shuffle_seed)
     elif dataset_name == "cifar100":
         num_classes = 100
         (X_train, y_train), (X_val, y_val) = tf.keras.datasets.cifar100.load_data()
-        return get_input(X_train, y_train, X_val, y_val, batch_size, shuffle_seed)
+        return get_input_keras(X_train, y_train, X_val, y_val, batch_size, shuffle_seed)
     elif dataset_name == "fashion_mnist":
         num_classes = 10
         (X_train, y_train), (X_val, y_val) = tf.keras.datasets.fashion_mnist.load_data()
         X_train = X_train.reshape((X_train.shape[0], 28, 28, 1))
         X_val = X_val.reshape((X_val.shape[0], 28, 28, 1))
-        return get_input(X_train, y_train, X_val, y_val, batch_size, shuffle_seed)
+        return get_input_keras(X_train, y_train, X_val, y_val, batch_size, shuffle_seed)
+    elif dataset_name == "cats_vs_dogs":
+        num_classes = 2
+        split = ["train[:70%]", "train[70%:]"]
+        train_dataset, val_dataset = tfds.load(
+            name="cats_vs_dogs", split=split, as_supervised=True
+        )
+        return get_input_tfds(train_dataset, val_dataset, batch_size, shuffle_seed)
